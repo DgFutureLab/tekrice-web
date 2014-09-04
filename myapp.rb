fa984dfa71302d433a6b3ce07703e44b5ddfe49e
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-09-04 17:27:17 +0900

    Initial commit

---
+++ myapp.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'json'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+get '/' do
+  erb :main
+end
+
+get '/dashboard' do
+  erb :dashboard
+end
+
+get '/dashboard/nodes' do
+  erb :nodes
+end
+
+get '/dashboard/settings' do
+  erb :settings
+end
+
+get '/test.json' do
+  data = { :location => "here", :data => "test data" }
+  response_data = data.to_json
+end
+
+get '/mapbox' do
+  test = "jojojojo"
+  erb :mapbox, locals:{foo: test}
+end
+
+get '/googlemap' do
+  erb :googlemap
+end
