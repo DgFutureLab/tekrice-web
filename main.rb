de42bcfe66644e0b3bc1e7669ae12d25ac25b2ff
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-09-16 18:08:17 +0900

    WIP on integrating server data

--- main.rb
+++ main.rb
@@ -22,12 +22,46 @@ get '/dashboard/nodes' do
 end
 
 get '/dashboard/nodes/:uuid' do
-  api_url = "http://128.199.191.249/reading/node_2/distance"
-  resp    = Net::HTTP.get_response( URI.parse(api_url) )
-  result  = resp.body
-  data    = JSON.parse(result)
+  # Placeholder for individual node data
+  #
+  #dist_api_url  = "http://128.199.191.249/reading/node_#{params[:uuid]}/distance"
+  #humid_api_url = "http://128.199.191.249/reading/node_#{params[:uuid]}/humidity"
+  #temp_api_url  = "http://128.199.191.249/reading/node_#{params[:uuid]}/temperature"
+  #dist_resp   = Net::HTTP.get_response( URI.parse(dist_api_url) )
+  #dist_result = dist_resp.body
+  #dist = JSON.parse(dist_result)
+  #humid_resp   = Net::HTTP.get_response( URI.parse(humid_api_url) )
+  #humid_result = humid_resp.body
+  #humid= JSON.parse(humid_result)
+  #temp_resp   = Net::HTTP.get_response( URI.parse(temp_api_url) )
+  #temp_result = temp_resp.body
+  #temp = JSON.parse(temp_result)
 
-  erb :nodedetail, locals:{ id:params[:uuid], data:data }
+  api_url = "http://128.199.191.249/nodes/all"
+  resp    = Net::HTTP.get_response( URI.parse(api_url) )
+  result  = JSON.parse(resp.body)
+
+  result.each do |d|
+    if d["id"].to_s == params[:uuid].to_s
+      @data = d
+      @data_temp = "N/A"
+      @data_humid = "N/A"
+      @data_dist = "N/A"
+      @data["sensors"].each do |readings|
+        if readings["alias"].to_s == "temperature"
+          @data_temp = readings["number of readings"].to_s
+        end
+        if readings["alias"].to_s == "humidity"
+          @data_humid = readings["number of readings"].to_s
+        end
+        if readings["alias"].to_s == "distance"
+          @data_dist = readings["number of readings"].to_s
+        end
+      end
+    end
+  end
+
+  erb :nodedetail, locals:{ id:params[:uuid], dist:@data_dist, humid:@data_humid, temp:@data_temp }
 end
 
 get '/dashboard/settings' do
