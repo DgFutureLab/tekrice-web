d27b626d1a15323325a145554fc8bd442119ecde
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-09-22 13:52:44 +0900

    Fixed errors on non-responsive API servers

--- main.rb
+++ main.rb
@@ -15,16 +15,28 @@ get '/dashboard' do
   temp_api_url  = "http://128.199.191.249/reading/node_2/temperature"
 
   dist_resp   = Net::HTTP.get_response( URI.parse(dist_api_url) )
-  dist_result = JSON.parse(dist_resp.body)
-  @dist = (!dist_result.nil? && !dist_result["data"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
+  if dist_resp.code == "200"
+    dist_result = JSON.parse(dist_resp.body)
+    @dist = (!dist_result.nil? && !dist_result["data"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
+  else
+    @dist = no_data
+  end
 
   humid_resp   = Net::HTTP.get_response( URI.parse(humid_api_url) )
-  humid_result = JSON.parse(humid_resp.body)
-  @humid= (!humid_result.nil? && !humid_result["data"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
+  if humid_resp.code == "200"
+    humid_result = JSON.parse(humid_resp.body)
+    @humid = (!humid_result.nil? && !humid_result["data"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
+  else
+    @humid = no_data
+  end
 
   temp_resp   = Net::HTTP.get_response( URI.parse(temp_api_url) )
-  temp_result = JSON.parse(temp_resp.body)
-  @temp = (!temp_result.nil? && !temp_result["data"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
+  if temp_resp.code == "200"
+    temp_result = JSON.parse(temp_resp.body)
+    @temp = (!temp_result.nil? && !temp_result["data"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
+  else
+    @temp = no_data
+  end
 
   erb :dashboard, locals:{ dist:@dist["data"][0]["value"], humid:@humid["data"][0]["value"], temp:@temp["data"][0]["value"] }
 end
@@ -44,16 +56,28 @@ get '/dashboard/nodes/:uuid' do
   temp_api_url  = "http://128.199.191.249/reading/node_#{params[:uuid]}/temperature"
 
   dist_resp   = Net::HTTP.get_response( URI.parse(dist_api_url) )
-  dist_result = JSON.parse(dist_resp.body)
-  @dist = (!dist_result.nil? && !dist_result["data"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
+  if dist_resp.code == "200"
+    dist_result = JSON.parse(dist_resp.body)
+    @dist = (!dist_result.nil? && !dist_result["data"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
+  else
+    @dist = no_data
+  end
 
   humid_resp   = Net::HTTP.get_response( URI.parse(humid_api_url) )
-  humid_result = JSON.parse(humid_resp.body)
-  @humid= (!humid_result.nil? && !humid_result["data"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
+  if humid_resp.code == "200"
+    humid_result = JSON.parse(humid_resp.body)
+    @humid = (!humid_result.nil? && !humid_result["data"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
+  else
+    @humid = no_data
+  end
 
   temp_resp   = Net::HTTP.get_response( URI.parse(temp_api_url) )
-  temp_result = JSON.parse(temp_resp.body)
-  @temp = (!temp_result.nil? && !temp_result["data"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
+  if temp_resp.code == "200"
+    temp_result = JSON.parse(temp_resp.body)
+    @temp = (!temp_result.nil? && !temp_result["data"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
+  else
+    @temp = no_data
+  end
 
   erb :nodedetail, locals:{ id:params[:uuid], dist:@dist["data"][0]["value"], humid:@humid["data"][0]["value"], temp:@temp["data"][0]["value"] }
 end
