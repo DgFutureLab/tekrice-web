b762f207e5e61e971977d44824a58e58a8fa22db
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-11-18 15:18:12 +0900

    Temp fix for /dashboard/nodes/:uuid

--- main.rb
+++ main.rb
@@ -73,16 +73,16 @@ get '/dashboard/nodes/:uuid' do
   # Placeholder for all node data
   #
   #api_url = "http://128.199.191.249/nodes/all"
-  no_data = {"data" => [{"value" => "N/A"}]}
+  no_data = {"objects" => [{"value" => "N/A"}]}
   
-  dist_api_url  = "http://128.199.191.249/reading/node_#{params[:uuid].to_s}/distance"
-  humid_api_url = "http://128.199.191.249/reading/node_#{params[:uuid]}/humidity"
-  temp_api_url  = "http://128.199.191.249/reading/node_#{params[:uuid]}/temperature"
+  dist_api_url  = "http://128.199.191.249/reading/node_1/distance"
+  humid_api_url = "http://128.199.191.249/reading/node_1/humidity"
+  temp_api_url  = "http://128.199.191.249/reading/node_1/temperature"
 
   dist_resp   = Net::HTTP.get_response( URI.parse(dist_api_url) )
   if dist_resp.code == "200"
     dist_result = JSON.parse(dist_resp.body)
-    @dist = (!dist_result.nil? && !dist_result["data"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
+    @dist = (!dist_result.nil? && !dist_result["objects"].empty? && dist_result["errors"].empty?) ? dist_result : no_data
   else
     @dist = no_data
   end
@@ -90,7 +90,7 @@ get '/dashboard/nodes/:uuid' do
   humid_resp   = Net::HTTP.get_response( URI.parse(humid_api_url) )
   if humid_resp.code == "200"
     humid_result = JSON.parse(humid_resp.body)
-    @humid = (!humid_result.nil? && !humid_result["data"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
+    @humid = (!humid_result.nil? && !humid_result["objects"].empty? && humid_result["errors"].empty?) ? humid_result : no_data
   else
     @humid = no_data
   end
@@ -98,12 +98,12 @@ get '/dashboard/nodes/:uuid' do
   temp_resp   = Net::HTTP.get_response( URI.parse(temp_api_url) )
   if temp_resp.code == "200"
     temp_result = JSON.parse(temp_resp.body)
-    @temp = (!temp_result.nil? && !temp_result["data"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
+    @temp = (!temp_result.nil? && !temp_result["objects"].empty? && temp_result["errors"].empty?) ? temp_result : no_data
   else
     @temp = no_data
   end
 
-  erb :nodedetail, locals:{ id:params[:uuid], dist:@dist["data"][0]["value"], humid:@humid["data"][0]["value"], temp:@temp["data"][0]["value"] }
+  erb :nodedetail, locals:{ id:params[:uuid], dist:@dist["objects"][0]["value"], humid:@humid["objects"][0]["value"], temp:@temp["objects"][0]["value"] }
 end
 
 get '/dashboard/settings' do
