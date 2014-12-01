0219b6bf851f2ff5e2ba829a3b388ecd618655e8
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-12-01 17:39:53 +0900

    WIP running cache in /dashboard

--- main.rb
+++ main.rb
@@ -12,12 +12,6 @@ get '/dashboard' do
   # Old no data object
   #no_data = {"objects" => [{"value" => "N/A"}]}
 
-  # Testing the new API server responses
-  all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
-  if all_data_call.code == "200"
-    @all_data = JSON.parse(all_data_call.body)
-  end
-
   # TEST until API server works
   test_data = 
     [{'latitude' => 35.143465822954,
@@ -57,11 +51,33 @@ get '/dashboard' do
       'longitude'=> 139.98876174814
      }]
 
+  # Checks cache file status before attempting to make API call
+
+  cache_file = File.join("cache", "alldata")
+
+  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*5))
+    all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
+
+    # Inserts new data into cache file
+    if all_data_call.code == "200"
+      @all_data = all_data_call.body
+      File.open(cache_file, "w"){ |f| f << @all_data }
+    else
+      if File.exist?(cache_file)
+        @all_data = File.read(cache_file)
+      else
+        @all_data = {"objects" => test_data}
+      end
+    end
+  else
+    @all_data = File.read(cache_file)
+  end
+
+  erb :dashboard, locals:{ data:JSON.parse(@all_data), json_data:@all_data }
+
+  # NOTES
   #test2 = Net::HTTP.get_response(URI.parse("http://128.199.191.249/reading/node_XX/distance&date_range=1week"))
-
   #erb :dashboard, locals:{ dist:@dist["objects"][0]["value"], humid:@humid["objects"][0]["value"], temp:@temp["objects"][0]["value"] }
-
-  erb :dashboard, locals:{ data:@all_data["objects"] }
 end
 
 get '/dashboard/nodes' do
@@ -165,29 +181,9 @@ get '/dashboard/map' do
   erb :map, locals:{ data:File.read(cache_file) }
 end
 
-get '/test_redis' do
-  # Testing the new API server responses
-  all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
-  if all_data_call.code == "200"
-    @all_data = all_data_call.body
-  end
-
-  cache_file = File.join("cache", "test")
-  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*5))
-    File.open(cache_file, "w"){ |f| f << @all_data }
-  end
-
-  erb :test_redis, locals:{ data:File.read(cache_file) }
-end
-
 get '/test/test.json' do
   data = { :location => "here", :data => "test data" }
   response_data = data.to_json
-end
-
-get '/test/mapbox' do
-  test = "jojojojo"
-  erb :mapbox, locals:{foo: test}
 end
 
 helpers do
