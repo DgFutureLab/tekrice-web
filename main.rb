16359f4b44083dc266f4840041201d1e44aebe47
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-12-03 12:24:42 +0900

    WIP put cache in /dashboard/map

--- main.rb
+++ main.rb
@@ -55,7 +55,7 @@ get '/dashboard' do
 
   cache_file = File.join("cache", "alldata")
 
-  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*5))
+  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*60))
     all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
 
     # Inserts new data into cache file
@@ -75,7 +75,7 @@ get '/dashboard' do
 
   erb :dashboard, locals:{ data:JSON.parse(@all_data), json_data:@all_data }
 
-  # NOTES
+  # NOTE so I don't forget the different API call response
   #test2 = Net::HTTP.get_response(URI.parse("http://128.199.191.249/reading/node_XX/distance&date_range=1week"))
   #erb :dashboard, locals:{ dist:@dist["objects"][0]["value"], humid:@humid["objects"][0]["value"], temp:@temp["objects"][0]["value"] }
 end
@@ -127,18 +127,6 @@ end
 
 get '/dashboard/map' do
 
-  # Testing the new API server responses
-  all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
-  if all_data_call.code == "200"
-    @all_data = all_data_call.body
-  end
-
-  # Check & creating cache file every 5 minutes
-  cache_file = File.join("cache", "alldata")
-  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*5))
-    File.open(cache_file, "w"){ |f| f << @all_data }
-  end
-
   # Dummy data for testing
   data =
     [{'latitude' => 35.143465822954,
@@ -178,7 +166,27 @@ get '/dashboard/map' do
       'longitude'=> 139.98876174814
      }]
 
-  erb :map, locals:{ data:File.read(cache_file) }
+  cache_file = File.join("cache", "alldata")
+
+  if !File.exist?(cache_file) || (File.mtime(cache_file) < (Time.now - 60*60))
+    all_data_call = Net::HTTP.get_response(URI.parse("http://128.199.191.249/node/all"))
+
+    # Inserts new data into cache file
+    if all_data_call.code == "200"
+      @all_data = all_data_call.body
+      File.open(cache_file, "w"){ |f| f << @all_data }
+    else
+      if File.exist?(cache_file)
+        @all_data = File.read(cache_file)
+      else
+        @all_data = {"objects" => test_data}
+      end
+    end
+  else
+    @all_data = File.read(cache_file)
+  end
+
+  erb :map, locals:{ data:@all_data }
 end
 
 get '/test/test.json' do
