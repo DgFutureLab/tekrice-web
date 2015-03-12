19e47a44e6e13211b46396a34bcb71cce7746ad1
Anditto Heristyo <anditto.heristyo@gmail.com>
2015-03-12 16:18:55 +0900

    Slightly better graph

--- static/data.js
+++ static/data.js
@@ -1,9 +1,12 @@
-var w = 600;
+// Visual padding
+var w = 700;
 var h = 300;
 var padding = 30;
 
+// Dataset
 var dataset = window.data;
 
+// Scales
 var xScale = d3.scale.linear()
                      .domain( [0, dataset.length - 1] )
                      .range( [padding, w - padding * 2] );
@@ -14,6 +17,7 @@ var yScale = d3.scale.linear()
                      })] )
                      .range( [h - padding, padding] );
 
+// Axis
 var xAxis = d3.svg.axis()
                   .scale(xScale)
                   .orient("bottom")
@@ -24,19 +28,13 @@ var yAxis = d3.svg.axis()
                   .orient("left")
                   .ticks(5);
 
+// Initialize the SVG
 var svg = d3.select("div#graph")
             .append("svg")
             .attr("width", w)
             .attr("height", h);
 
-svg.append("clipPath")
-   .attr("id", "chart-area")
-   .append("rect")
-   .attr("x", padding)
-   .attr("y", padding)
-   .attr("width", w - padding * 3)
-   .attr("height", h - padding * 2);
-
+// Circles
 svg.append("g")
    .attr("id", "circles")
    .attr("clip-path", "url(#chart-area)")
@@ -51,9 +49,10 @@ svg.append("g")
      return yScale(d.value);
    })
    .attr("r", 5)
+   .attr("fill", "blue")
    .on("mouseover", function(d) {
-     var xPosition = parseFloat( d3.select(this).attr("x") ) / 2 + w / 2;
-     var yPosition = parseFloat( d3.select(this).attr("y") ) / 2 + h / 2;
+     var xPosition = parseFloat( d3.select(this).attr("cx") );
+     var yPosition = parseFloat( d3.select(this).attr("cy") ) + 100 ;
 
      d3.select("#infobox")
        .style("left", xPosition + "px")
@@ -66,6 +65,19 @@ svg.append("g")
      d3.select("#infobox").classed("hidden", true);
    });
 
+// Lines
+var lineFunction = d3.svg.line()
+                         .x(function(d) { return xScale(d.index); })
+                         .y(function(d) { return yScale(d.value); })
+                         .interpolate("linear");
+
+var lineGraph = svg.append("path")
+                   .attr("d", lineFunction(dataset))
+                   .attr("stroke", "blue")
+                   .attr("stroke-width", 1)
+                   .attr("fill", "none");
+
+// Appending Axis
 svg.append("g")
    .attr("class", "axis")
    .attr("transform", "translate(0," + (h - padding) + ")")
